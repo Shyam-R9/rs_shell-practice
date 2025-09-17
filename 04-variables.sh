84d208a2b921441d7a292a60d5ca5666eab7b9a5
shyam <shyam9.r9@gmail.com>
2025-09-17 22:27:21 +0530

    variabls-part2

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: How are you $PERSON2"
+echo "$PERSON2:: great !! How are you"
+echo "$PERSON1:: Likewise"
+echo "$PERSON2:: Lets catchup tomorrow"
+echo "$PERSON1:: Sure"
