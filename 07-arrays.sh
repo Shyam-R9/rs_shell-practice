20e575410b3f6c5079a4562501f57c15b7afdd2a
shyam <shyam9.r9@gmail.com>
2025-09-17 22:46:01 +0530

    7_arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,6 +3,6 @@
 MOVIES=("animal" "arjun reddy" "kabir singh")
 
 echo "first_movie: ${MOVIES[0]}"
-echo "second_movie: $(MOVIES[1])"
+echo "second_movie: ${MOVIES[1]}"
 echo "all movies: ${MOVIES[@]}"
 
