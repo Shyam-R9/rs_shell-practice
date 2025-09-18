0cb2e343806d92c49ce3c0a925a789e61492bfe7
shyam <shyam9.r9@gmail.com>
2025-09-18 17:36:37 +0530

    11 and 12

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#check if the current user is the root user
+
+if [ "$(id -u )" -ne 0 ]; then
+    echo "Error: You do not have privileges to run the script"
+    exit 1
+fi
+
+is_package_already_installed() {
+    if ! dnf list installed $1 &> /dev/null: then
+    echo "mysql is not found"
+    else
+    echo "MySql is already installed"
+    exit 0
+    fi
+}
+
+#Insall the package and verify the installation 
+install_package() {
+    if dnf install mysql -y; then
+        echo "mysql installed successfully"
+    
+    else
+        echo "mysql installation failed"
+        exit 1
+}
+
