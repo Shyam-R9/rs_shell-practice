5bb671d79a784b333f22d8e5b8f51da42ca97d56
shyam <shyam9.r9@gmail.com>
2025-09-18 07:24:29 +0530

    8_special variables

---
+++ 08-special variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Name of the script: $0"
+echo "No of arguments passed to the script: $#"
+echo "Home directory of the user running the script: $HOME"
+echo "User running the script: $USER"
+sleep 10 &
+echo "PID of the script: $$"
+echo "PID of the last command: $!"
+echo "All variables passed to the script: $@"
